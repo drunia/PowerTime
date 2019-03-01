9bdb056bec8b2d88c016e72461c45cafc9b6c99f
drunia <druniax@gmail.com>
2019-03-02 01:36:00 +0200

    some fixes

--- ui/main.py
+++ ui/main.py
@@ -12,6 +12,8 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self._setup_ui()
+        self.find_plugins(plugins_dir="../plugins")
+
 
     def _setup_ui(self):
         self.setWindowTitle("PowerTime")
@@ -31,18 +33,14 @@ class MainWindow(QMainWindow):
         menubar.addMenu(self.menu_devices)
         self.statusBar().showMessage("*(&&*&$#")
 
-        self._build_devices_actions(plugin_dir="../plugins")
 
-    def _build_devices_actions(self, plugin_dir="./plugins"):
+    def find_plugins(self, plugins_dir="./plugins"):
         """search device-plugins in modules dir
-        :return list[QAction]"""
+        :return list[Plugin_Class...,]"""
         import pkgutil, inspect
-
-        self.actions = []
-        self.modules = []
-
+        plugins = []
         # Find modules with classes inherited from PTBasePlugin
-        mod_info_list = list(pkgutil.iter_modules([plugin_dir]))
+        mod_info_list = list(pkgutil.iter_modules([plugins_dir]))
         for module in mod_info_list:
             m = __import__("plugins." + module.name, fromlist=['object'])
             clslist = inspect.getmembers(m, inspect.isclass)
@@ -50,13 +48,14 @@ class MainWindow(QMainWindow):
                 bases = cls[1].__mro__
                 for b in bases:
                     if b.__name__ == "PTBasePlugin" and not cls[1].__name__ == "PTBasePlugin":
-                        print("{}(): Plugin class detected: {}".format(inspect.stack()[0][3], cls[1].__name__))
-                        self.modules.append(cls[1])
+                        print("{}(): Plugin class finded: {}".format(inspect.stack()[0][3], cls[1].__name__))
+                        plugins.append(cls[1])
                         break
+        del pkgutil, inspect
+        return plugins
 
-        print(dir())
-
-
+    def _build_devices_actions(self):
+        self.find_plugins()
 
 
 if __name__ == "__main__":
